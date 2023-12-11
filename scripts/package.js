abbbc6793e0e7f33bed73544c2a8d8e8852f656e
jmattaa <mattajonathan1@gmail.com>
2023-12-11 20:43:36 +0100

    packaging script gets icons
    It can also create for other platforms than the one you're using
    
    Signed-off-by: jmattaa <mattajonathan1@gmail.com>

--- scripts/package.js
+++ scripts/package.js
@@ -1,13 +1,40 @@
 const packager = require('electron-packager');
 const path = require('path');
 
+const args = process.argv.slice(2);
+const platformOptionIndex = args.indexOf('--platform');
+const archOptionIndex = args.indexOf('--arch');
+
+const platform = platformOptionIndex !== -1 ? args[platformOptionIndex + 1] : process.platform;
+const arch = archOptionIndex !== -1 ? args[archOptionIndex + 1] : process.arch;
+
+const validPlatforms = ['darwin', 'win32', 'linux'];
+const validArchitectures = ['x64', 'ia32'];
+
+if (!validPlatforms.includes(platform) || !validArchitectures.includes(arch)) {
+    console.error('Invalid platform or architecture. Please provide valid options.');
+    process.exit(1);
+}
+
+function getPlatformIconPath() {
+    switch (platform) {
+        case 'darwin':
+            return path.join(process.cwd(), 'assets/icons/icon.icns');
+        case 'win32':
+            return path.join(process.cwd(), 'assets/icons/icon.ico');
+        case 'linux':
+            return path.join(process.cwd(), 'assets/icons/icon.png');
+    }
+}
+
 const appConfig = {
     dir: process.cwd(),
     name: 'Glide',
-    platform: process.platform, 
-    arch: process.arch, 
-    out: path.join(process.cwd(), 'releases'), 
-    overwrite: true, 
+    platform: platform,
+    arch: arch,
+    out: path.join(process.cwd(), 'releases'),
+    overwrite: true,
+    icon: getPlatformIconPath(),
 };
 
 packager(appConfig)
@@ -15,6 +42,6 @@ packager(appConfig)
         console.log(`App packaged successfully at: ${appPaths.join(', ')}`);
     })
     .catch(error => {
-        console.error('Error packaging the app:', error);
+        console.error('Error packaging the app!\n', error);
     });
 
